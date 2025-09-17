72ca736e5261a74db3f4a115c9e8380043c67f14
Dima <dimongotovsky@mail.ru>
2025-09-17 09:05:59 +0700

    parse_file

--- main.py
+++ main.py
@@ -83,3 +83,22 @@ def read_lines(file_path: str) -> list:
     Returns:        Список строк файла    """
     with open(file_path, "r") as f:
         return f.readlines()
+
+
+def parse_file(file_path: str, parser_func) -> list[dict] | None:
+    """Обрабатывает файл с использованием указанной функции-парсера.
+
+    Args:        file_path: Путь к файлу для обработки        parser_func: Функция для парсинга строк    """
+    results = []
+    try:
+        for line in read_lines(file_path):
+            line = line.strip()
+            if line:
+                results.append(parser_func(line))
+
+        return results
+
+    except FileNotFoundError:
+        print(f"Ошибка: файл {file_path} не найден")
+    except Exception as e:
+        print(f"Ошибка при обработке файла {file_path}: {e}")

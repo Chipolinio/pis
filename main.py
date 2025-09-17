91c62739ddda631c5506e4ea5512dec60dfedf57
Dima <dimongotovsky@mail.ru>
2025-09-17 09:03:58 +0700

    parse_currency_rate

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def parse_currency_rate(input_str: str) -> dict:
+    """Парсит строку с информацией о курсе.
+
+        Args:            input_str: Строка для парсинга в формате "from_currency" "to_currency" "rate" "date"
+        Returns:            Словарь с ключами: from_currency to_currency rate date    """
     parts = input_str.strip().split()
     currency1 = parts[0].replace('"', '')
     currency2 = parts[1].replace('"', '')
     rate = float(parts[2])
     date = parts[3]
-    return {"from_currency": currency1,"to_currency": currency2,"rate": rate,"date": date}
-with open("1.txt", "r") as f:
-    for data in f:
-        print(parse_currency_rate(data))
+
+    return {"type": "currency_rate",
+            "from_currency": currency1,
+            "to_currency": currency2,
+            "rate": rate,
+            "date": date}
+

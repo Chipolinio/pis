81853735ba16777265aacb82b036aa2b8af63eb3
Dima <dimongotovsky@mail.ru>
2025-09-23 15:59:35 +0700

    доработка main

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from parse import parse_currency_rate, parse_historical_rate, parse_rate_with_source, parse_rate_with_fee
-from file_manaher import parse_file
-
+from file_manager import process_file
+from Parser import Parser
 
 def main() -> None:
+    parser = Parser()
     file_parsers = [
-        ("1.txt", parse_currency_rate),
-        ("2.txt", parse_historical_rate),
-        ("3.txt", parse_rate_with_source),
-        ("4.txt", parse_rate_with_fee),
+        ('1.txt', parser.parse_currency_rate, "Курсы валют"),
+        ('2.txt', parser.parse_historical_rate, "Исторические курсы"),
+        ('3.txt', parser.parse_rate_with_source, "Курсы с источником"),
+        ('4.txt', parser.parse_rate_with_fee, "Курсы с комиссией")
     ]
 
-    for file_path, func in file_parsers:
-        objects = parse_file(file_path, func)
-        for obj in objects:
-            print(obj)
+    for file_path, parser_func, data_name in file_parsers:
+        process_file(file_path, parser_func, data_name)
 
-    print()
+    print("=== Сводная информация ===")
+    print(f"Всего записей с обычным курсом: {len(parser.currency_rate)}")
+    print(f"Всего записей за диапазон дат: {len(parser.historical_rate)}")
+    print(f"Всего записей с источником: {len(parser.rate_with_source)}")
+    print(f"Всего записей с комиссией: {len(parser.rate_with_fee)}")
+    print(f"Всего валютных записей: {len(parser.get_all_value())}")
+
 
 
 if __name__ == "__main__":

32dcccd353fb947973d82b72a2cbd2ad41eae282
Dima <dimongotovsky@mail.ru>
2025-09-23 15:58:59 +0700

    библиотека классов с ошибками

---
+++ errors.py
@@ -0,0 +1,6 @@
+class ParsingError(Exception):
+    """Ошибка при парсинге входных данных."""
+
+
+class ValidationError(Exception):
+    """Ошибка валидации данных (например, неверная дата или валюта)."""
